75573b6bbb1fe0d5688739e930733f07f36d5633
Doom <clement.doumergue@epitech.eu>
2017-11-23 16:36:39 +0100

    fix docker container options

--- docker/set_root_pw.sh
+++ docker/set_root_pw.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
 if [ -z "${SSH_KEY}" ]; then
-	echo "=> Please pass your public key in the SSH_KEY environment variable"
-	exit 1
+    echo "=> Please pass your public key in the SSH_KEY environment variable"
+    exit 1
 fi
 
-for MYHOME in /root /home/doom /home/blowa; do
-	echo "=> Adding SSH key to ${MYHOME}"
-	mkdir -p ${MYHOME}/.ssh
-	chmod go-rwx ${MYHOME}/.ssh
-	echo "${SSH_KEY}" > ${MYHOME}/.ssh/authorized_keys
-	chmod go-rw ${MYHOME}/.ssh/authorized_keys
-	echo "=> Done!"
-done
-chown -R doom:doom /home/doom/.ssh
-chown -R blowa:blowa /home/blowa/.ssh
+echo "=> Adding SSH key to /root"
+mkdir -p /root/.ssh
+chmod go-rwx /root/.ssh
+echo "${SSH_KEY}" > /root/.ssh/authorized_keys
+chmod go-rw /root/.ssh/authorized_keys
+echo "=> Done!"
 
 echo "========================================================================"
-echo "You can now connect to the container through SSH using:"
+echo "            You can now connect to the container through SSH"
 echo ""
-echo "    ssh -p <port> <user>@<host>"
-echo ""
-echo "Users: root (full access), doom/blowa (limited user account)."
+echo "      Users: root (full access), doom/blowa (regular user account)"
 echo "========================================================================"
